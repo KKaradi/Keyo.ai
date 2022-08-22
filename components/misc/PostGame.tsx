aca762494014b2b79c9d021421070488b502cf06
Kai Karadi <100025564@mvla.net>
2022-08-21 22:36:04 -0700

    reolad change

--- components/misc/PostGame.tsx
+++ components/misc/PostGame.tsx
@@ -8,7 +8,10 @@ const PostGame: NextPage = () => {
   return (
     <div className={styles.div}>
       <div className={styles.text}>
-        You&apos;re playing yesterday&apos;s game.
+        You&apos;re playing yesterday&apos;s game. <br />
+        <div className={styles.smallText}>
+          (You won&apos;t be able to win any rewards)
+        </div>
       </div>
       <div className={styles.button} onClick={() => window.location.reload()}>
         Click for next game
